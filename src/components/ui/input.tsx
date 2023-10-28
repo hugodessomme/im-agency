f49358a213228a208f5e1f4aeefbd273ee0da92e
Hugo Dessomme <hdessomme@gmail.com>
2023-10-28 19:02:10 +0200

    style: edit controls height

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "file:font-medium flex h-10 w-full rounded-md border border-background-subtle bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
+          "file:font-medium flex h-12 w-full rounded-md border border-background-subtle bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
           className
         )}
         ref={ref}
